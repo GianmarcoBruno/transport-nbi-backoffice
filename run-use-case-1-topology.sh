1ad82964b46d58ce038727c464b49f80d50f6781
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2017-12-04 17:30:38 +0100

    validation scripts point to the tranport-nbi repository

--- run-use-case-1-topology.sh
+++ run-use-case-1-topology.sh
@@ -2,4 +2,4 @@
 echo "###################################################################"
 echo "ietf-te-topology has local modifications wrt to the public version."
 echo "###################################################################"
-../json-yang/validate ietf-te-topology.yang use-case-1-topology-00.json data
+../json-yang/validate ietf-te-topology.yang ../transport-nbi/Internet-Drafts/Use-Case-1-Analysis/01/use-case-1-topology-01.json data
