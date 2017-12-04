8dc9df18494515d01b76600bc107ffa17372ee08
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2017-12-04 16:32:21 +0100

    removed unused files

--- transport-nbi/run-use-case-1-topology.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "###################################################################"
-echo "ietf-te-topology has local modifications wrt to the public version."
-echo "###################################################################"
-../../validate ietf-te-topology.yang use-case-1-topology-00.json data
