70f69c89eee79ff838469453f6f327b58e85a7bb
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2018-05-09 11:30:36 +0200

    PR29/use-case-1-topology-01 COMPLETED

--- use-case-1-topology-get/run.sh
+++ use-case-1-topology-get/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-echo "###################################################################"
-echo "ietf-te-topology has local modifications wrt to the public version."
-echo "###################################################################"
-JSON=use-case-1-topology-01.json
+
+# usage: run.sh
+# override defaults: e.g.
+# FETCH=y ./run.sh
+
+JSON=mpi1-otn-topology.json
 echo "JSON instance: " $JSON
+
+if [ -z "$FETCH" ]
+then
+   echo 'will use local models for validation'
+else
+   echo 'will download models from the network'
+   ../../json-yang/scripts/fetchModels $JSON
+fi
+
 ../../json-yang/validate $JSON data
