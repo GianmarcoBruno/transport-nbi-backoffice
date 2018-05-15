f1288aa7188d0127bb5dd8caf8321a05b724807f
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2018-05-15 14:03:43 +0200

    use-case-1-topology-get: uses the latest validate therefore removed the check for FETCH

--- use-case-1-topology-get/run.sh
+++ use-case-1-topology-get/run.sh
@@ -6,13 +6,4 @@
 
 JSON=mpi1-otn-topology.json
 echo "JSON instance: " $JSON
-
-if [ -z "$FETCH" ]
-then
-   echo 'will use local models for validation'
-else
-   echo 'will download models from the network'
-   ../../json-yang/scripts/fetchModels $JSON
-fi
-
 ../../json-yang/validate $JSON data
