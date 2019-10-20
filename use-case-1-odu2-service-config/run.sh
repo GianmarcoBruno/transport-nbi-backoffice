d050b00ca26a4507f2758c5b1ce5d92895f28e5f
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2019-10-20 13:23:16 +0200

    odu2-service-config fixes validates - includes fix to ietf-otn-tunnel

--- use-case-1-odu2-service-config/run.sh
+++ use-case-1-odu2-service-config/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -i
 
-#validate -w data -j mpi1-odu2-service-config.json -f -k
-#validate -w data -j mpi1-odu2-service-config.json -y downloads -k -s yanglint
+# currently we need a module with local modifications because it is broken
+
+# so if you run from scratch
+# validate -w data -j mpi1-odu2-service-config.json -s yanglint -y downloads -v -f
+# then revert to use local modificationsA
+# git checkout -- downloads/ietf-otn-tunnel@2019-07-08.yang
+# and run again
+# validate -w data -j mpi1-odu2-service-config.json -s yanglint -y downloads -v -f
+
+# if you have already downloaded models, just:
+validate -w data -j mpi1-odu2-service-config.json -s yanglint -k -y downloads
