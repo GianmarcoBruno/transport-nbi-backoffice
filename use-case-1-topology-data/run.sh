0b1e181016aa761a2db29170158c1e2d1b66159f
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2019-10-20 16:46:23 +0200

    mpi1-otn-topology.json validates with yanglint that wants empty leaf as "" instead of [null]

--- use-case-1-topology-data/run.sh
+++ use-case-1-topology-data/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -i
 
 #validate -w data -j mpi1-otn-topology.json -f
-#validate -w data -j mpi1-otn-topology.json -s yanglint -y downloads
+validate -w data -j mpi1-otn-topology.json -s yanglint -y downloads -v -f
