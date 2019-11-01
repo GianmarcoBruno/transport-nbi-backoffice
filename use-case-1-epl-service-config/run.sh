9c1106edf84d6877ff13b2391103b098b1761236
Gianmarco Bruno <gianmarco.bruno@ericsson.com>
2019-11-01 10:27:59 +0100

    epl-service-config validates

--- use-case-1-epl-service-config/run.sh
+++ use-case-1-epl-service-config/run.sh
@@ -3,7 +3,4 @@
 # usage: run.sh
 
 # validates
-#validate -w data -j mpi1-epl-service-config.json -f
-
-# currently does not validate
-validate -w data -j mpi1-epl-service-config.json -f -s yanglint
+validate -w data -j mpi1-epl-service-config.json -y downloads -s yanglint -v -f
